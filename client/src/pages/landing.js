056544baeb1a5871c9c456627e39aff61f402e8a
Shawn Wright <volushawn@gmail.com>
2021-08-17 23:12:48 -0500

    landing page styling

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -41,7 +41,7 @@ const Landing = () => {
   return (
     <Container fluid={true} className='text-center'>
       <h1 className='display-2 mt-3'>Fantasy Bazaar</h1>
-      <Row className='mt-5 px-5'>
+      <Row className='mt-4 px-5'>
         <Col>
           <ButtonGroup aria-label='Landing Page Actions'>
             <A href='/newusertype' alt='Get Started' className='text-center'>
@@ -57,35 +57,35 @@ const Landing = () => {
           </ButtonGroup>
         </Col>
       </Row>
-      <Row className='align-items-end mt-5'>
+      <Row className='align-items-end mt-4'>
         <Col className='align-self-end'>
           <A href='/about' alt='About Fantasy Bazaar'>
             <Button variant='secondary'>About</Button>
           </A>
         </Col>
       </Row>
-      <Row className='mt-3'>
-        <Col>
+      <Row className='mt-4'>
+        <Col xs={6} md={{span: 4, offset: 2}} className="mb-2">
           <A href='/pf1example' alt='Example Pathfinder Bazaar'>
             <Card className='text-white text-center'>
               <Card.Img
-                src='https://lorempixel.com/g/150/150/abstract'
+                src='https://lorempixel.com/g/100/100/abstract'
                 alt='Card image'
               />
               <Card.ImgOverlay>
                 <Card.Title>
-                  <h2>Sample Pathfinder Bazaar</h2>
+                  Sample Pathfinder Bazaar
                 </Card.Title>
                 <Card.Text></Card.Text>
               </Card.ImgOverlay>
             </Card>
           </A>
         </Col>
-        <Col>
+        <Col xs={6}  md={{span: 4, offset: 0}} >
           <A href='/dndexample' alt='Example Dungeons and Dragons Bazaar'>
             <Card className='text-white text-center'>
               <Card.Img
-                src='https://lorempixel.com/g/150/150/abstract'
+                src='https://lorempixel.com/g/100/100/abstract'
                 alt='Card image'
               />
               <Card.ImgOverlay>
